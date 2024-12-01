8968a91500cd66896b4a2918cec41b61c45c968c
Sebastian Müller <seb.mueller.91@web.de>
2024-12-01 10:24:49 +0100

    Solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (leftList, rightList) = getSortedLists(input)
+
+        var diff = 0
+        leftList.forEachIndexed { index, leftEntry ->
+            val rightEntry = rightList[index]
+            diff += Math.abs(leftEntry - rightEntry)
+        }
+        return diff
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (leftList, rightList) = getSortedLists(input)
+
+        var similarity = 0
+        leftList.forEachIndexed { index, leftEntry ->
+            similarity += leftEntry * rightList.count { it == leftEntry }
+        }
+        return similarity
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun getSortedLists(input: List<String>): Pair<List<Int>, List<Int>> {
+    val leftList = mutableListOf<String>()
+    val rightList = mutableListOf<String>()
+
+    input.forEach { line ->
+        val parts = line.split(' ').filter { it.isNotBlank() }
+        leftList.add(parts[0])
+        rightList.add(parts[1])
+    }
+
+    leftList.sort()
+    rightList.sort()
+
+    val leftListSorted = leftList.map { it.toInt() }
+    val rightListSorted = rightList.map { it.toInt() }
+
+    return Pair(leftListSorted, rightListSorted)
 }
