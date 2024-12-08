0c7c5145012cc1a6faa4fd681a8edbbb2f858708
Sebastian Müller <seb.mueller.91@web.de>
2024-12-08 20:33:51 +0100

    Refactor day08-1

--- src/Day08.kt
+++ src/Day08.kt
@@ -12,11 +12,15 @@ private sealed class Cell {
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.parse().fillAntinodes().flatten().count { it.isAntinode }
+        val grid = input.parse()
+        val antennas = grid.getAntennasPositions()
+        return grid.fillAntinodes(antennas).flatten().count { it.isAntinode }
     }
 
     fun part2(input: List<String>): Int {
-        return input.parse().fillAntinodes().flatten().count { it.isAntinode }
+        val grid = input.parse()
+        val antennas = grid.getAntennasPositions()
+        return grid.fillAntinodes(antennas).flatten().count { it.isAntinode }
     }
 
     val testInput = readInput("Day${DAY.toDayString()}_test")
@@ -24,69 +28,64 @@ fun main() {
     check(part1(testInput) == 14)
 
     val input = readInput("Day${DAY.toDayString()}")
+    part1(input).println()
     check(part1(input) == 367)
     check (part2(testInput) == 34)
     part1(input).println()
     part2(input).println()
 }
 
-private fun MutableList<MutableList<Position>>.fillAntinodes(): MutableList<MutableList<Position>> {
-    forEachIndexed { x, row ->
-        row.forEachIndexed { y, _ ->
-            if (isAntinode1(x, y)) {
-                this[x][y] = this[x][y].copy(isAntinode = true)
+private fun MutableList<MutableList<Position>>.getAntennasPositions(): Map<Char, MutableList<Pair<Int, Int>>> {
+    val map = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
+    for (r in indices) {
+        for (c in this[0].indices) {
+            val cell = this[r][c].cell
+            if (cell is Cell.Antenna) {
+                map.getOrPut(cell.frequency) { mutableListOf() }
+                    .add(Pair(r,c))
+            }
+        }
+    }
+    return map
+}
+
+private fun MutableList<MutableList<Position>>.printGrid() {
+    for (row in this) {
+        for (position in row) {
+            when {
+                position.cell is Cell.Antenna && position.isAntinode -> print("!")
+                position.isAntinode -> print("#")
+                position.cell is Cell.Antenna -> print(position.cell.frequency)
+                else -> print(".")
+            }
+        }
+        kotlin.io.println()
+    }
+}
+
+private fun MutableList<MutableList<Position>>.fillAntinodes(antennas: Map<Char, MutableList<Pair<Int, Int>>>): MutableList<MutableList<Position>> {
+    antennas.forEach { (_, positions) ->
+        positions.forEachIndexed { i1, p1 ->
+            for (i2 in i1+1 until positions.size) {
+                val (r1, c1) = p1
+                val (r2, c2) = positions[i2]
+
+                val dr = r2 - r1
+                val dc = c2 - c1
+
+                markAntinode(r1-dr, c1 - dc)
+                markAntinode(r2+dr, c2 + dc)
             }
         }
     }
     return this
 }
 
-private fun MutableList<MutableList<Position>>.isAntinode1(row: Int, col: Int): Boolean {
-    for (r1 in indices) {
-        for (c1 in this[0].indices) {
-            if (r1 == row && c1 == col) {
-                continue
-            }
-            val (r2, c2) = Pair(row + 2 * (r1 - row), col + 2 * (c1 - col))
-
-            val pos1 = this[r1][c1]
-            val pos2 = this.getOrNull(r2, c2) ?: continue
-
-            if (pos1.cell is Cell.Antenna
-                && pos2.cell is Cell.Antenna
-                && pos1.cell.frequency == pos2.cell.frequency
-            ) {
-                return true
-            }
-        }
+private fun MutableList<MutableList<Position>>.markAntinode(row: Int, col: Int) {
+    if (row in indices && col in this[0].indices) {
+        this[row][col] = this[row][col].copy(isAntinode = true)
     }
-    return false
 }
-
-private fun MutableList<MutableList<Position>>.isAntinode2(row: Int, col: Int): Boolean {
-    for (r1 in indices) {
-        for (c1 in this[0].indices) {
-            if (r1 == row && c1 == col) {
-                continue
-            }
-            val (r2, c2) = Pair(row + 2 * (r1 - row), col + 2 * (c1 - col))
-
-            val pos1 = this[r1][c1]
-            val pos2 = this.getOrNull(r2, c2) ?: continue
-
-            if (pos1.cell is Cell.Antenna
-                && pos2.cell is Cell.Antenna
-                && pos1.cell.frequency == pos2.cell.frequency
-            ) {
-                return true
-            }
-        }
-    }
-    return false
-}
-
-private fun MutableList<MutableList<Position>>.getOrNull(row: Int, col: Int): Position? =
-    if (row in indices && col in this[0].indices) this[row][col] else null
 
 private fun List<String>.parse(): MutableList<MutableList<Position>> = map {
     it.map { char ->
