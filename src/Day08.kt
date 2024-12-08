6bcb99256bf38d54edf23efd933b520f788a5d63
Sebastian Müller <seb.mueller.91@web.de>
2024-12-08 20:50:50 +0100

    Solve 8-2

--- src/Day08.kt
+++ src/Day08.kt
@@ -20,17 +20,15 @@ fun main() {
     fun part2(input: List<String>): Int {
         val grid = input.parse()
         val antennas = grid.getAntennasPositions()
-        return grid.fillAntinodes(antennas).flatten().count { it.isAntinode }
+        return grid.fillAntinodes2(antennas).flatten().count { it.isAntinode }
     }
 
     val testInput = readInput("Day${DAY.toDayString()}_test")
-    part1(testInput).println()
     check(part1(testInput) == 14)
 
     val input = readInput("Day${DAY.toDayString()}")
-    part1(input).println()
     check(part1(input) == 367)
-    check (part2(testInput) == 34)
+    check(part2(testInput) == 34)
     part1(input).println()
     part2(input).println()
 }
@@ -42,7 +40,7 @@ private fun MutableList<MutableList<Position>>.getAntennasPositions(): Map<Char,
             val cell = this[r][c].cell
             if (cell is Cell.Antenna) {
                 map.getOrPut(cell.frequency) { mutableListOf() }
-                    .add(Pair(r,c))
+                    .add(Pair(r, c))
             }
         }
     }
@@ -66,26 +64,69 @@ private fun MutableList<MutableList<Position>>.printGrid() {
 private fun MutableList<MutableList<Position>>.fillAntinodes(antennas: Map<Char, MutableList<Pair<Int, Int>>>): MutableList<MutableList<Position>> {
     antennas.forEach { (_, positions) ->
         positions.forEachIndexed { i1, p1 ->
-            for (i2 in i1+1 until positions.size) {
+            for (i2 in i1 + 1 until positions.size) {
                 val (r1, c1) = p1
                 val (r2, c2) = positions[i2]
 
                 val dr = r2 - r1
                 val dc = c2 - c1
 
-                markAntinode(r1-dr, c1 - dc)
-                markAntinode(r2+dr, c2 + dc)
+                markAntinode(Pair(r1 - dr, c1 - dc))
+                markAntinode(Pair(r2 + dr, c2 + dc))
             }
         }
     }
     return this
 }
 
-private fun MutableList<MutableList<Position>>.markAntinode(row: Int, col: Int) {
-    if (row in indices && col in this[0].indices) {
-        this[row][col] = this[row][col].copy(isAntinode = true)
+private fun MutableList<MutableList<Position>>.fillAntinodes2(antennas: Map<Char, MutableList<Pair<Int, Int>>>): MutableList<MutableList<Position>> {
+    antennas.forEach { (_, positions) ->
+        positions.forEachIndexed { i1, a1 ->
+            for (i2 in i1 + 1 until positions.size) {
+                val a2 = positions[i2]
+
+                val (r1, c1) = a1
+                val (r2, c2) = a2
+
+                val dr = r2 - r1
+                val dc = c2 - c1
+
+                markAllAntinodes(a1, a2, Pair(dr, dc))
+            }
+        }
+    }
+    return this
+}
+
+private fun MutableList<MutableList<Position>>.markAntinode(pos: Pair<Int, Int>) {
+    val (r, c) = pos
+
+    if (isValid(pos)) {
+        this[r][c] = this[r][c].copy(isAntinode = true)
     }
 }
+
+private fun MutableList<MutableList<Position>>.markAllAntinodes(
+    a1: Pair<Int, Int>,
+    a2: Pair<Int, Int>,
+    d: Pair<Int, Int>
+) {
+    var p = a1
+    while (isValid(p)) {
+        markAntinode(p)
+        p = Pair(p.first + d.first, p.second + d.second)
+    }
+
+    p = a1
+    while (isValid(p)) {
+        markAntinode(p)
+        p = Pair(p.first - d.first, p.second - d.second)
+    }
+
+}
+
+private fun MutableList<MutableList<Position>>.isValid(p: Pair<Int, Int>): Boolean =
+    p.first in indices && p.second in this[0].indices
 
 private fun List<String>.parse(): MutableList<MutableList<Position>> = map {
     it.map { char ->
